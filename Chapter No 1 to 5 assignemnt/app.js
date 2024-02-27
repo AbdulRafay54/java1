c1e92b98b7428c63842a8e73b2b5b07701b1caa0
AbdulRafay54 <139426616+AbdulRafay54@users.noreply.github.com>
2024-02-27 19:12:14 +0500

    Add files via upload

---
+++ Chapter No 1 to 5 assignemnt/app.js
@@ -0,0 +1,93 @@
+                               //  Chapter No 01 //
+
+ //question no 1//
+
+alert("Welcome")
+
+  //question no 2//
+  alert("Error! Please enter a valid password")
+
+  // question 3
+alert("Welcome to JS Land..! \nHappy coding!");
+
+// question 4
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// question 5
+alert("Hello.. I can run JS through my web browser's console");
+
+                                                  // Chapter 2 ---------------------------------
+
+// question 1
+var username;
+username = "Abdul Rafay";
+
+// question 2
+var myName = "Abdul Rafay";
+
+// question 3
+var message = "hello world";
+alert(message);
+
+// question 4
+var nam = "Abdul Rafay";
+var age = "16 years old";
+var course = "Certified Mobile Application Development";
+
+alert(nam);
+alert(age);
+alert(course);
+
+// question 5
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(pizza);
+
+// question 6
+var email ="rafayabdul7817@gmail.com";
+alert("My email address is" + " " + email);
+
+// question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book" + " " + email);
+
+// question 8
+document.write("Yah! I can write HTML content through JavaScript");
+
+// question 9
+var pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(pattern)
+
+
+
+
+               // Chapter 3 ---------------------------------
+
+// question 1
+var age = 15;
+alert("I am" + " " + age + " " + "years old")
+
+// question 2
+var visit = "You have visited this site 14 times";
+alert(visit);
+
+// question 3
+var birthYear = 2007;
+document.write("My birth year is" + " " + birthYear + "<br>");
+document.write("Data type of my declared variable is" + " " + birthYear)
+
+// question 4
+var visitorName = "John Doe";
+var tiltle = "T-shirt";
+var quantity = 5;
+
+document.write("<b>" + visitorName + "</b>" + " " + "ordered" + " " + "<b>" + quantity + " " + tiltle + "</b>" + " on XYZ clothing store" );
+
+// Chapter 4 ---------------------------------
+
+// question 1
+var name,age,course;
+name = "Abdul Rafay";
+age = "16";
+course ="Web and Mobile";
+
